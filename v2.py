bc0fedd24754fb75fe10babcf43d7e2ab15c0fbb
Tomasz Tomanek <tymancjo@Mac-mini-Tomasz.local>
2021-05-28 22:48:20 +0200

    Reading of csv

--- v2.py
+++ v2.py
@@ -1,6 +1,10 @@
 import cv2
 import random
 import copy
+import sys
+import time
+import csv
+import numpy as np
 
 def play_videoFile(filePath):
 
@@ -15,34 +19,45 @@ def play_videoFile(filePath):
     #     ret_val, frame = cap.read()
     #     vid_buffer.append(frame)
 
-    # font
+    # text related variables
     font = cv2.FONT_HERSHEY_SIMPLEX
-    # org
     org = (50, 50)
-    # fontScale
     fontScale = 1
     color = (255, 255, 255)
-    # Line thickness of 2 px
     thickness = 1
        
 
 
+    buffer_mem = 500 #[MB]
+    buffer_size = 100 #[frames]
     frm = 0    
     total_frame = 0
     prev_frm = -1
     frm_step = 1
-    mark = ["[ ]","[>]","[<]"] 
+    mark = ["[-]","[>]","[<]"] 
+    t_start = 0
+    t_end = 1
+
 
     while True:
+        fps = int(1 / (t_end - t_start))
+        t_start = time.time()
 
         if frm_step > 0 and frm == len(vid_buffer):
             ret_val, frame = cap.read()
             vid_buffer.append(frame)
-            total_frame += frm_step 
 
+            if total_frame == 0:
+                frame_mem_size = sys.getsizeof(vid_buffer[-1])
+                print(f"Frame memory size: {frame_mem_size} Bytes")
+                buffer_size = int( buffer_mem / (frame_mem_size / (1024*1024)) )
+                print(f"Buffer set to {buffer_size} frames")
+
+
+            total_frame += frm_step 
             buffer_len = len(vid_buffer)
 
-            if buffer_len > 100:
+            if buffer_len > buffer_size:
                 del vid_buffer[0]
 
         buffer_len = len(vid_buffer)
@@ -52,7 +67,7 @@ def play_videoFile(filePath):
         display_frame = copy.copy(vid_buffer[frm])
 
         # Using cv2.putText() method
-        txt_string = f"{mark[frm_step]} AbsFrame: {total_frame+frm-buffer_len+1}  BufferFrm: {frm} HeadPos: {total_frame}"
+        txt_string = f"{mark[frm_step]} AbsFrame: {total_frame+frm-buffer_len+1}  BufferFrm: {frm} HeadPos: {total_frame} FPS: {fps}"
 
         prev_frm = frm
         frm += frm_step
@@ -91,10 +106,47 @@ def play_videoFile(filePath):
         elif the_pressed_key == ord('l'):
             frm_step = 1
 
+        t_end = time.time()
+
     cv2.destroyAllWindows()
 
+def get_csv_data(csv_file, skip=8, delimiter=';'):
+
+    with open(csv_file, 'r', newline = '') as file:
+
+        reader = csv.reader(file, delimiter = delimiter)
+        row_cnt = 0
+
+        data_set = []
+        for row in reader:
+            row_cnt += 1
+
+            data_row = []
+            if row_cnt > skip:
+                # processing the data row.
+                if len(row) > 0:
+                    for d in row:
+                        temp_data = d.replace(',','.') 
+                        try:
+                            temp_data = float(temp_data)
+                        except Exception as e:
+                            temp_data = 0
+
+                        data_row.append((temp_data))
+
+                data_set.append(data_row)
+            else:
+                print(row)
+
+
+    data_set = np.array(data_set)
+    print(data_set[:,0])
+
+
+
 def main():
-    play_videoFile('/Users/tymancjo/LocalGit/video/sc_data_example/11435.mp4')
+    get_csv_data('/Users/tymancjo/LocalGit/video/sc_data_example/11435.txt')
+    # play_videoFile('/Users/tymancjo/LocalGit/video/sc_data_example/11435.mp4')
 
 if __name__ == '__main__':
     main()
